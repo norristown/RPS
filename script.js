26be7a91374bfd12fac288ae30907fcb3bd1d7ad
norristown <jarvasity@gmail.com>
2023-02-17 03:36:12 -0500

    Added a 'scoreboard' and additional rounds to play

--- script.js
+++ script.js
@@ -26,62 +26,88 @@ function getPlayerChoice() {
 }
 // getPlayerChoice();
 
-
+let playerWin = 0;
+let computerWin = 0;
+let round = 0;
 
 function playRound (player, computer) {
     if (player === 0 && computer === 1) {
-        return "You Lose.";
+        return round++, computerWin++, 'you lose';
     } else if (player === 0 && computer === 2) {
-        return "You Win! Rock beats Scissors.";
+        return round++, playerWin++, 'you win';
     } else if (player === 1 && computer === 0) {
-        return "You Win! Paper beats Rock.";
+        return round++, playerWin++, 'you win';
     } else if (player === 1 && computer === 2) {
-        return "You Lose.";
+        return round++, computerWin++, 'you lose';
     } else if (player === 2 && computer === 0) {
-        return "You Lose.";
+        return round++, computerWin++, 'you lose';
     } else if (player === 2 && computer === 1) {
-        return "You Win! Scissors beat Paper.";
-    } else {
+        return round++, playerWin++, 'you win';
+    } else if (player === computer) {
         return "It's a Draw. Try again rookie biotch";
+    } else {
+        return "Refresh";
     }
 }
 
+// let player = getPlayerChoice();
+// let computer = getComputerChoice1();
+//  console.log(playRound(player, computer));
+// if (player === 0 ) {
+//     console.log("You: Rock");
+// } else if (player === 1) {
+//     console.log("You: Paper");
+// } else {
+//     console.log("You: Scissors");
+// }
 
-
-const player = getPlayerChoice();
-const computer = getComputerChoice1();
-if (player === 0 ) {
-    console.log("You: Rock");
-} else if (player === 1) {
-    console.log("You: Paper");
-} else {
-    console.log("You: Scissors");
-}
-
-if (computer === 0 ) {
-    console.log("Computer: Rock");
-} else if (computer === 1) {
-    console.log("Computer: Paper");
-} else {
-    console.log("Computer: Scissors");
-}
+// if (computer === 0 ) {
+//     console.log("Computer: Rock");
+// } else if (computer === 1) {
+//     console.log("Computer: Paper");
+// } else {
+//     console.log("Computer: Scissors");
+// }
 
 
 // console.log(player, computer);
-console.log(playRound(player, computer));
+// console.log(playRound(player, computer));
 
-// if (player === 0 && computer === 1) {
-//     console.log("You Lose.");
-// } else if (player === 0 && computer === 2) {
-//     console.log("You Win! Rock beats Scissors.");
-// } else if (player === 1 && computer === 0) {
-//     console.log("You Win! Paper beats Rock.");
-// } else if (player === 1 && computer === 2) {
-//     console.log("You Lose.");
-// } else if (player === 2 && computer === 0) {
-//     console.log("You Lose.");
-// } else if (player === 2 && computer === 1) {
-//     console.log("You Win! Scissors beat Paper.");
+
+
+function game() {
+    let player = getPlayerChoice();
+    let computer = getComputerChoice1();
+    console.log(playRound(player, computer));
+    return (`Round: ${round}
+Your wins: ${playerWin}
+Evil Computer wins: ${computerWin}`);
+}
+
+function repeat() {
+    for (let i = 0; i < 5; i++){
+    console.log(game());
+    }
+}
+repeat();
+    
+
+    
+
+// function score() {
+// if (playRound(player, computer) === 'You Lose.') {
+//     computerWin++;
+//     round++;
 // } else {
-//     console.log("It's a Draw. Try again rookie biotch");
+//     playerWin++;
+//     round++;
 // }
+
+// return (`Round: ${round}
+// Your wins: ${playerWin}
+// Evil Computer wins: ${computerWin}`);
+// }
+
+
+
+
